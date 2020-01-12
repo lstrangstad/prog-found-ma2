5299e1f8d86d41f902806a88101a6e1ef9256809
lstrangstad <57751647+lstrangstad@users.noreply.github.com>
2020-01-12 22:29:36 +0100

    Add files via upload
    Module assignment 2 by Linus Strangstad

---
+++ script.js
@@ -0,0 +1,45 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+console.log(petArray[1]);
+
+// question 3
+petArray.push("sheep");
+console.log(petArray);
+
+// question 4
+console.log(petArray.length);
+
+// question 5
+var catObject = {
+    name: "Tiger",
+    colour: "black",
+    age: 8
+}
+
+// question 6
+console.log(catObject.age);
+
+// question 7
+var catArray = [
+    {
+        name: "Tiger",
+        colour: "black",
+        age: 8   
+    }
+];
+
+// question 8
+for(var i = 0; i < catArray.length; i++) {
+    console.log(catArray[i]);
+}
+
+// question 9
+function logToConsole(name) {
+    console.log("Hello " + name);
+}
+
+// question 10
+logToConsole("Donkey");
+
